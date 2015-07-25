db79a726cee18dd63b1c490b5cc4243727c3d658
Davide Cavaliere <cavaliere.davide@gmail.com>
2015-07-25 13:52:00 +0200

    error controller just display and error message and die

--- application/controllers/error.php
+++ application/controllers/error.php
@@ -14,11 +14,8 @@ class Error extends Controller
      * PAGE: index
      * This method handles the error page that will be shown when a page is not found
      */
-    public function index()
+    public function index($message)
     {
-        // load views
-        require APP . 'views/_templates/header.php';
-        require APP . 'views/error/index.php';
-        require APP . 'views/_templates/footer.php';
+        die($message);
     }
 }
