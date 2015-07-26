5260fcaf311e97a166b1ceca4b49dd1f96829f00
Davide Cavaliere <cavaliere.davide@gmail.com>
2015-07-26 13:46:28 +0200

    removing unnecessary code

--- application/model/model.php
+++ application/model/model.php
@@ -17,14 +17,6 @@ class Model
      */
     function __construct()
     {
-      // $username = "root";
-      // $password = "my-secret-pw";
-      // $hostname = "mysql";
-      //
-      // //connection to the database
-      // $dbhandle = mysql_connect($hostname, $username, $password)
-      //   or die("Unable to connect to MySQL");
-      // echo "Connected to MySQL<br>";
       $this->openDatabaseConnection();
     }
 
@@ -42,60 +34,11 @@ class Model
 
         // generate a database connection, using the PDO connector
         // @see http://net.tutsplus.com/tutorials/php/why-you-should-be-using-phps-pdo-for-database-access/
-          $this->db = new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS, $options);
         try {
+          $this->db = new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS, $options);
         } catch (PDOException $e) {
           die('Database connection could not be established.');
         }
     }
 
-
-    /**
-     * Get all songs from database
-     */
-    public function getAll()
-    {
-        $sql = "SELECT id, artist, track, link FROM song";
-        $query = $this->db->prepare($sql);
-        $query->execute();
-
-        // fetchAll() is the PDO method that gets all result rows, here in object-style because we defined this in
-        // core/controller.php! If you prefer to get an associative array as the result, then do
-        // $query->fetchAll(PDO::FETCH_ASSOC); or change core/controller.php's PDO options to
-        // $options = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC ...
-        return $query->fetchAll();
-    }
-
-
-    /**
-     * Get a song from database
-     */
-    public function getOne($song_id)
-    {
-        $sql = "SELECT id, artist, track, link FROM song WHERE id = :song_id LIMIT 1";
-        $query = $this->db->prepare($sql);
-        $parameters = array(':song_id' => $song_id);
-
-        // useful for debugging: you can see the SQL behind above construction by using:
-        // echo '[ PDO DEBUG ]: ' . debugPDO($sql, $parameters);  exit();
-
-        $query->execute($parameters);
-
-        // fetch() is the PDO method that get exactly one result
-        return $query->fetch();
-    }
-
-    /**
-     * Get simple "stats". This is just a simple demo to show
-     * how to use more than one model in a controller (see application/controller/songs.php for more)
-     */
-    public function count()
-    {
-        $sql = "SELECT COUNT(id) AS amount_of_songs FROM song";
-        $query = $this->db->prepare($sql);
-        $query->execute();
-
-        // fetch() is the PDO method that get exactly one result
-        return $query->fetch()->amount_of_songs;
-    }
 }
--- index.php
+++ index.php
@@ -1,15 +1,5 @@
 <?php
 
-/**
- * A simple PHP MVC skeleton
- *
- * @package php-mvc
- * @author Panique
- * @link http://www.php-mvc.net
- * @link https://github.com/panique/php-mvc/
- * @license http://opensource.org/licenses/MIT MIT License
- */
-
 // set a constant that holds the project's folder path, like "/var/www/".
 // DIRECTORY_SEPARATOR adds a slash to the end of the path
 define('ROOT', __DIR__ . DIRECTORY_SEPARATOR);
@@ -18,11 +8,6 @@ define('APP', ROOT . 'application' . DIRECTORY_SEPARATOR);
 
 define('PAGE', ROOT . 'page' . DIRECTORY_SEPARATOR);
 define('LIBS', APP . 'libs' . DIRECTORY_SEPARATOR);
-// This is the (totally optional) auto-loader for Composer-dependencies (to load tools into your project).
-// If you have no idea what this means: Don't worry, you don't need it, simply leave it like it is.
-if (file_exists(ROOT . 'vendor/autoload.php')) {
-    require ROOT . 'vendor/autoload.php';
-}
 
 // load application config (error reporting etc.)
 require APP . '/config/config.php';
