77878893ccfa90b5a210484537640c505e9a59fb
Davide Cavaliere <cavaliere.davide@gmail.com>
2015-07-27 12:10:22 +0200

    removing unnecessary code

--- application/model/model.php
+++ application/model/model.php
@@ -15,7 +15,6 @@ class Model
       $this->openDatabaseConnection();
     }
 
-
     /**
      * Open the database connection with the credentials from application/config/config.php
      */
@@ -24,11 +23,9 @@ class Model
         // set the (optional) options of the PDO connection. in this case, we set the fetch mode to
         // "objects", which means all results will be objects, like this: $result->user_name !
         // For example, fetch mode FETCH_ASSOC would return results like this: $result["user_name] !
-        // @see http://www.php.net/manual/en/pdostatement.fetch.php
         $options = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 
         // generate a database connection, using the PDO connector
-        // @see http://net.tutsplus.com/tutorials/php/why-you-should-be-using-phps-pdo-for-database-access/
         try {
           $this->db = new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS, $options);
         } catch (PDOException $e) {
